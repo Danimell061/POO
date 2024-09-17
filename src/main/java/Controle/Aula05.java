78d0cf6e576a8109911853fd1a9bf7ec9bbb516f
Danimell <dani9ct@gmail.com>
2024-09-17 11:02:11 -0300

    Creat

---
+++ src/main/java/Controle/Aula05.java
@@ -0,0 +1,21 @@
+package Controle;
+
+public class Aula05 {
+    
+    public static void main(String[] args) {
+        String string1 = "TI Catolica";
+        String string2 = "ti catolica"; 
+        int comparacao = string1.compareTo(string2);
+        
+        if (comparacao > 0) {
+        System.out.println("string1 é maior que string2");
+        } 
+        else if (comparacao < 0) {
+        System.out.println("string1 é menor que string2");
+        } 
+        else {
+        System.out.println("string1 é igual a string2");
+        }
+
+    }
+}
