83106466b412570465839c9bf1b1f3cd72478fbc
Danimell <dani9ct@gmail.com>
2024-09-24 09:55:38 -0300

    Criando classes

---
+++ Aula07/src/main/java/RevisaoPOO/RevisaoPOO.java
@@ -0,0 +1,10 @@
+package RevisaoPOO;
+import Modelo.Gerente;
+
+public class RevisaoPOO {
+
+    public static void main(String[] args) {
+        Gerente gestor = new Gerente();
+        
+    }
+}
