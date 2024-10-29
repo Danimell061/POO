b1069bacad6d565fa5fcca5a7b01b3a654ebb34b
Danimell <dani9ct@gmail.com>
2024-10-29 11:12:28 -0300

    Desafio - ARRAY

--- Aula08/src/main/java/visao/Exibir.java
+++ Aula08/src/main/java/visao/Exibir.java
@@ -7,8 +7,10 @@ import javax.swing.JOptionPane;
 public class Exibir {
     public static void main(String[] args) {
         Conteudo pessoa = new Conteudo();
-        pessoa.setNome(JOptionPane.showInputDialog(null,"Digite o nome do aluno"));
+        pessoa.setNome(JOptionPane.showInputDialog(null, "Digite o nome do aluno", "Exemplo: Daniel"));
         
         JOptionPane.showMessageDialog(null, "O nome da pessoa é "+ pessoa.getNome());
+        
+        //Desafio - Armazenar os 5 nomes nesta estrtura de array;
     }
 }
