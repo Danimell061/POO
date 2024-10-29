83e2a410e0deea0c36f07ad2dc845a85cb3d6400
Danimell <dani9ct@gmail.com>
2024-10-29 10:11:14 -0300

    Aula08

---
+++ Aula08/src/main/java/Modelo/Aula.java
@@ -0,0 +1,16 @@
+package Modelo;
+
+public class Aula {
+    public static void main(String[] args) {
+        int[] idade = new int[5];
+        idade[0] = 20;
+        idade[1] = 15;
+        idade[2] = 25;
+        idade[3] = 35;
+        idade[4] = 45;
+        System.out.println("A idade encontra-se " 
+                            + "na posição 2 "+ idade[2]);
+        
+    }
+    
+}
