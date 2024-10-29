ea55b9c6447f62046af0aea8c81c7e8181193ea4
Danimell <dani9ct@gmail.com>
2024-10-29 10:24:41 -0300

    funcões

--- Aula08/src/main/java/Modelo/Aula.java
+++ Aula08/src/main/java/Modelo/Aula.java
@@ -4,12 +4,28 @@ public class Aula {
     public static void main(String[] args) {
         int[] idade = new int[5];
         idade[0] = 20;
-        idade[1] = 15;
+        idade[1] = 10;
         idade[2] = 25;
         idade[3] = 35;
         idade[4] = 45;
-        System.out.println("A idade encontra-se " 
-                            + "na posição 2 "+ idade[2]);
+        
+        int soma = idade[0] + idade[1];
+        System.out.println("A soma é " + soma);
+        
+        int sub = idade[0] - idade[1];
+        System.out.println("A subtração é " + sub);
+        
+        int mult = idade[0] * idade[1];
+        System.out.println("A multiplicação é " + mult);
+        
+        int div = idade[0] / idade[1];
+        System.out.println("A divisão é " + div);
+        
+        for(int i=0;i<5;i++){
+            System.out.println("A idade encontra-se " 
+                            + "na posição "+ (i+1) + " é "+ idade[i]);
+        }
+        
         
     }
     
