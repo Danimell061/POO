0a0b505d2667af38d40e66734eb1120ce8b1a14c
Danimell <dani9ct@gmail.com>
2024-10-29 09:34:13 -0300

    Update RevisaoPOO.java

--- Aula07/src/main/java/RevisaoPOO/RevisaoPOO.java
+++ Aula07/src/main/java/RevisaoPOO/RevisaoPOO.java
@@ -4,11 +4,6 @@ import Modelo.*;
 public class RevisaoPOO {
 
     public static void main(String[] args) {
-        Gerente gestor = new Gerente();
-        gestor.setNome("João da Silva");
-        gestor.setSalario(30000.0);
-        System.out.println(gestor.getNome());
-        System.out.println(gestor.getBonificacaoG());
-        
+
     }
 }
